96f245355e085a0404e14c4d631b8a17983ffede
unknown <kevinken@souescoteiro.org.br>
2019-10-27 15:27:22 -0300

    Basic API - version without middlewares

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projectList = [{id: 1, title: 'kevin', tasks:[]}, {id: 2, title: 'jose', tasks:[]}];
+
+server.post('/projects', (req, res) => {
+    const {id, title} = req.body;
+
+    const project = {
+        id,
+        title,
+        tasks: []
+    };
+
+    projectList.push(project);
+
+    return res.json(project);
+});
+
+server.get('/projects', (req, res) => {
+    res.json(projectList);
+});
+
+server.put('/projects/:id', (req, res) => {
+    const {id} = req.params;
+    const {title} = req.body;
+
+    const index = projectList.findIndex(proj => {
+        return proj.id == id;
+    });
+
+    const project = projectList[index];
+
+    project.title = title;
+
+    res.json(project);
+});
+
+server.delete('/projects/:id', (req, res) => {
+    const {id} = req.params;
+
+    const index = projectList.findIndex(proj => {
+        return proj.id == id;
+    });
+
+    const deleted = projectList.splice(index, 1);
+
+    res.json(deleted);
+});
+
+server.put('/projects/:id/tasks', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const index = projectList.findIndex(proj => {
+        return proj.id == id;
+    });
+
+    const project = projectList[index];
+
+    project.tasks.push(title);
+
+    res.json(project);
+
+});
+
+server.listen(3000);
+
+function checkId (req, res, next){
+    
+}
